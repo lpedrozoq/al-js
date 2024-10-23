e064692893cd518a14163673ce0bd426713e6f07
Leonel Pedrozo <leonel.pedrozo.quiroz@gmail.com>
2024-10-22 22:15:45 -0500

    Nuevos ejericios

--- src/async/04-promesas-fetch-lista-products.js
+++ src/async/04-promesas-fetch-lista-products.js
@@ -1,14 +1,13 @@
-import fetch from 'node-fetch';
-const API  = "https://api.escuelajs.co/api/v1";
+import fetch from "node-fetch";
+const API = "https://api.escuelajs.co/api/v1";
 
 console.log(":: node-fetch-list-products ::");
 
-function fetchData(urlAPI){
-    return fetch(urlAPI);
-} 
+function fetchData(urlAPI) {
+  return fetch(urlAPI);
+}
 
 fetchData(`${API}/products`)
-    .then(response => response.json())  //Aquí obtengo toda la respuesa general
-    .then(products => console.log(products))
-    .catch(error=>console.error(error));
-
+  .then((response) => response.json()) //Aquí obtengo toda la respuesa general
+  .then((products) => console.log(products))
+  .catch((error) => console.error(error));
